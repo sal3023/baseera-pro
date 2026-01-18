61251cc02efd6a609e57d943dcc7b8236b38cf00
sal3023 <opnseeamo@gmail.com>
2026-01-18 17:48:12 +0300

    Deep Repair Deployment: v12.0 Final Production

---
+++ tailwind.config.js
@@ -0,0 +1,2 @@
+/** @type {import('tailwindcss').Config} */
+export default { content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], theme: { extend: { fontFamily: { sans: ['Tajawal', 'sans-serif'] } } }, plugins: [], };
