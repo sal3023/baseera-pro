1ec79bb0f2bcb35547b920133efb282d99c20ba0
sal3023 <opnseeamo@gmail.com>
2026-01-18 17:23:06 +0300

    Final Sync v7.0: Mirroring full source code

--- index.tsx
+++ index.tsx
@@ -1,1 +1,5 @@
-import React from 'react'; import ReactDOM from 'react-dom/client'; import App from './App.tsx'; ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(<App />);
